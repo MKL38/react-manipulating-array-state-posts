e9a1cc4323e9de1c5bf10df1fbe99c0983d0b87a
Pom <kasiphit@gmail.com>
2025-03-24 14:40:23 +0700

    complete react-manipulating-array-state-posts assignment

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,29 +1,62 @@
+import { useState } from "react";
+import { postData } from "../raw-data/post-data";
+
 function Posts() {
+  const [postList, setPostList] = useState(postData);
+
+  const handleLike = (postId) => {
+    const newPostList = postList.map((post) => 
+      post.id === postId ? 
+      { ...post, likes: post.likes + 1 } 
+      : post
+    );
+    setPostList(newPostList);
+  };
+
+  const handleDisLike = (postId) => {
+    const newPostList = postList.map((post) => (
+      (post.id === postId && post.likes > 0)? 
+      { ...post, likes: post.likes - 1 } 
+      : post
+    ));
+    setPostList(newPostList);
+  };
+
   return (
-    <div class="app-wrapper">
-      <h1 class="app-title">Posts</h1>
-      <div class="post-list">
-        <div class="post-item">
-          <div class="post-header">
-            <h2>Post Title #1</h2>
-            <div class="post-social-media-stats">
-              <span class="stats-topic">Likes: </span>
-              <span class="post-likes">10</span>
+    <div className="app-wrapper">
+      <h1 className="app-title">Posts</h1>
+
+      {/* Post Board */}
+      {postList.map((item) => (
+      <div className="post-list" key={item.id}>
+        <div className="post-item">
+          <div className="post-header">
+            <h2>{item.title}</h2>
+            <div className="post-social-media-stats">
+              <span className="stats-topic">Likes: </span>
+              <span className="post-likes">{item.likes}</span>
             </div>
           </div>
-          <p class="post-content">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
-            vel turpis vestibulum, aliquet ipsum vitae, auctor risus. Morbi
-            tincidunt, leo non molestie consectetur, elit libero faucibus
-            tellus, sed fringilla tortor libero sit amet odio. Maecenas sed ante
-            condimentum mauris euismod pellentesque eu eu justo...
+          <p className="post-content">
+            {item.content}
           </p>
-          <div class="post-actions">
-            <button class="like-button">Like</button>
-            <button class="dislike-button">Dislike</button>
+          <div className="post-actions">
+            <button 
+              className="like-button"
+              onClick={() => handleLike(item.id)}
+              >
+                Like
+            </button>
+            <button 
+              className="dislike-button"
+              onClick={() => handleDisLike(item.id)}
+              >
+                Dislike
+            </button>
           </div>
         </div>
       </div>
+      ))}
     </div>
   );
 }
