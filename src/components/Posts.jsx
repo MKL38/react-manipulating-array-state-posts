0f09a1689630cdf794bcd33e20528c46d4e2f2ac
Don Teerapat <futhepteerapat@gmail.com>
2025-03-24 15:36:00 +0700

    feat:add Array State Posts Assignment

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,29 +1,54 @@
+import { useState } from "react";
+import { postData } from "../raw-data/post-data";
+
 function Posts() {
+  const [postList, setpostList] = useState(postData);
+
+  const addLikes = (postid) => {
+    const addPostList = postList.map((post) => {
+      if (post.id === postid) {
+        return { ...post, likes: post.likes + 1 };
+      }
+      return post;
+    });
+    setpostList(addPostList);
+  };
+
+  const deleteLike = (postid) => {
+    const deletePostList = postList.map((post) => {
+      if (post.id === postid && post.likes > 0) {
+        return { ...post, likes: post.likes - 1 };
+      }
+      return post;
+    });
+    setpostList(deletePostList);
+  };
+
   return (
-    <div class="app-wrapper">
-      <h1 class="app-title">Posts</h1>
-      <div class="post-list">
-        <div class="post-item">
-          <div class="post-header">
-            <h2>Post Title #1</h2>
-            <div class="post-social-media-stats">
-              <span class="stats-topic">Likes: </span>
-              <span class="post-likes">10</span>
+    <div className="app-wrapper">
+      <h1 className="app-title">Posts</h1>
+      {postList.map((item) => (
+        <div className="post-list" key={item.id}>
+          <div className="post-item">
+            <div className="post-header">
+              <h2>{item.title}</h2>
+              <div className="post-social-media-stats">
+                <span className="stats-topic">Likes: </span>
+                <span className="post-likes">{item.likes}</span>
+              </div>
+            </div>
+            <p className="post-content">{item.content}</p>
+            <div className="post-actions">
+              <button className="like-button" onClick={() => addLikes(item.id)}>
+                Like
+              </button>
+              <button className="dislike-button" onClick={() => deleteLike(item.id)}>
+                Dislike
+              </button>
             </div>
           </div>
-          <p class="post-content">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
-            vel turpis vestibulum, aliquet ipsum vitae, auctor risus. Morbi
-            tincidunt, leo non molestie consectetur, elit libero faucibus
-            tellus, sed fringilla tortor libero sit amet odio. Maecenas sed ante
-            condimentum mauris euismod pellentesque eu eu justo...
-          </p>
-          <div class="post-actions">
-            <button class="like-button">Like</button>
-            <button class="dislike-button">Dislike</button>
-          </div>
         </div>
-      </div>
+      ))}
     </div>
   );
 }
