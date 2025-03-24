c1cbe621f571f540e0018040b39aed41285759cf
ritchie-gr8 <rit.yotsathon@gmail.com>
2025-03-24 13:52:52 +0700

    feat: render post by post-data and implement like/dislike handle function

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,28 +1,52 @@
+import { useState } from "react";
+import { postData } from "../raw-data/post-data";
+
 function Posts() {
+  const [posts, setPosts] = useState(postData);
+
+  const updateLikes = (postId, isLike) => {
+    setPosts((prev) => {
+      return prev.map((post) =>
+        post.id === postId
+          ? {
+              ...post,
+              likes: isLike ? post.likes + 1 : Math.max(0, post.likes - 1),
+            }
+          : post
+      );
+    });
+  };
+
   return (
     <div class="app-wrapper">
       <h1 class="app-title">Posts</h1>
       <div class="post-list">
-        <div class="post-item">
-          <div class="post-header">
-            <h2>Post Title #1</h2>
-            <div class="post-social-media-stats">
-              <span class="stats-topic">Likes: </span>
-              <span class="post-likes">10</span>
+        {posts.map((post) => (
+          <div class="post-item" key={post.id}>
+            <div class="post-header">
+              <h2>{post.title}</h2>
+              <div class="post-social-media-stats">
+                <span class="stats-topic">Likes: </span>
+                <span class="post-likes">{post.likes}</span>
+              </div>
+            </div>
+            <p class="post-content">{post.content}</p>
+            <div class="post-actions">
+              <button
+                class="like-button"
+                onClick={() => updateLikes(post.id, true)}
+              >
+                Like
+              </button>
+              <button
+                class="dislike-button"
+                onClick={() => updateLikes(post.id, false)}
+              >
+                Dislike
+              </button>
             </div>
           </div>
-          <p class="post-content">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
-            vel turpis vestibulum, aliquet ipsum vitae, auctor risus. Morbi
-            tincidunt, leo non molestie consectetur, elit libero faucibus
-            tellus, sed fringilla tortor libero sit amet odio. Maecenas sed ante
-            condimentum mauris euismod pellentesque eu eu justo...
-          </p>
-          <div class="post-actions">
-            <button class="like-button">Like</button>
-            <button class="dislike-button">Dislike</button>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
